74ae708635c003a08192b16db5a7bf28027ce14b
Felix Madutsa <fmadutsa@princeton.edu>
2017-04-10 23:18:17 -0400

    trying to add exec hashing files

--- kernel/execdrafting.c
+++ kernel/execdrafting.c
@@ -72,7 +72,7 @@ int calculate_hash(struct task_struct *p) {
  	printk("\n");
  	printk("the mode %d\n", (int)file->f_mode);
  	printk("\n");
- 	/*vfs_read(file, (void __user *) ehdr, sizeof(Elf32_Ehdr), &position); */
+ 	vfs_read(file, (void __user *) ehdr, sizeof(Elf32_Ehdr), &position); 
  	/* correct until here */
  	
  	printk("Correct here 1 \n");
